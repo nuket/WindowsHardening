8da6f85cafd250a08f482afe5b28e3fbaa19b18d
Max Vilimpoc <no-reply@vilimpoc.org>
2019-03-20 16:50:14 +0100

    Add Azure build number to PDF content

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-asciidoctor-pdf --out-file WindowsHardening.pdf -a build-badge= -a allow-uri-read README.asciidoc
+asciidoctor-pdf --out-file WindowsHardening.pdf -a allow-uri-read -a build-badge= -a build-number=SMOKEBUILD README.asciidoc
